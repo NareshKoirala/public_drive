9f0dea7710a011fb8850618a6905ea353f9bc8d1
NareshKoirala <chelseanaresh10@gmail.com>
2026-07-10 09:19:27 -0600

    Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def Welcome():
+    return "Welcome to my fastapi api. This api is build to pass local drive information to user ui"
+
+
+@app.post("/auth/{password}")
+def auth(password):
+    return
