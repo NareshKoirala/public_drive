912d846d89cdebaeb616024e8d2eb3929d020d51
NareshKoirala <chelseanaresh10@gmail.com>
2026-07-16 10:00:34 -0600

    feat: implement findDrives function to list block devices

--- main/subprocFunc.py
+++ main/subprocFunc.py
@@ -1,5 +1,23 @@
 import subprocess
+import json
+from osFunc import createDir
 
 
-def find():
-    pass
+def findDrives():
+    drives = []
+
+    r = subprocess.run(["lsblk", "-Jo", "NAME,PATH"], capture_output=True, text=True)
+    d = json.loads(r.stdout)
+
+    for dr in d.get("blockdevices"):
+        if dr.get("name").startswith("sd"):
+            drives.append(dr.get("children"))
+
+    return drives
+
+
+def mountDrives(root):
+    drs = findDrives()
+
+    for dr in drs:
+         pass
